c7a0fa7c58f23632ceb206a5c270848653cad6be
Fang Liu <lfxjtu@gmail.com>
2023-04-16 12:10:34 +1200

    add greeting controller and read value from application.properties

---
+++ src/main/java/io/lfxjtu/springbootconfig/GreetingController.java
@@ -0,0 +1,19 @@
+package io.lfxjtu.springbootconfig;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Value("${my.greeting}")
+    private String greetingMessage;
+
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return greetingMessage;
+    }
+}
